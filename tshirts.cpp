c8fce3171eb3ecbf22fda8d4a92d3389294887bd
Thribhuvan Gupta S <sthribhuvan.gupta@philips.com>
2023-08-25 09:40:20 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -5,11 +5,11 @@ namespace TShirtSize{
     char size(int cms)
     {
         char sizeName = '\0';
-        if (cms < 38)
+        if (cms <= 38)
         {
             sizeName = 'S';
         }
-        else if (cms > 38 && cms < 42)
+        else if (cms > 38 && cms <= 42)
         {
             sizeName = 'M';
         }
